77bb0a81d22fc06c760752754af7824f5aba2121
Arnaldo Diaz <arnaldo.e.diaz@gmail.com>
2022-08-08 13:25:47 -0400

    added category component turned shop-data into a js file, added nexted routing to shop component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import {
   getAuth,
   signInWithRedirect,
@@ -9,19 +10,30 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBgOslrj6gg6djD4c5nTbk400Fo5faxcT4',
-  authDomain: 'crwn-db-48047.firebaseapp.com',
-  databaseURL: 'https://crwn-db-48047.firebaseio.com',
-  projectId: 'crwn-db-48047',
-  storageBucket: 'crwn-db-48047.appspot.com',
-  messagingSenderId: '120995028956',
-  appId: '1:120995028956:web:57b404d4044a67a1',
+  apiKey: 'AIzaSyDgScZjBppGIhkElB8gJMrZwpLVWSJlIrc',
+  authDomain: 'crwn-db-v2-1319f.firebaseapp.com',
+  projectId: 'crwn-db-v2-1319f',
+  storageBucket: 'crwn-db-v2-1319f.appspot.com',
+  messagingSenderId: '515169381700',
+  appId: '1:515169381700:web:46f54ec95ac6e64207bb11',
+  measurementId: 'G-DBBTV8BZ9E',
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -29,13 +41,54 @@ googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
 
-export const auth = getAuth();
+export const auth = getAuth(app);
 export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
-export const db = getFirestore();
+export const db = getFirestore(app);
+
+/* App's functionality */
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  try {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+      const docRef = doc(collectionRef, object.title.toLowerCase());
+      batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('Sucess: Added Collection and Documents');
+  } catch (ex) {
+    console.error('Something went wrong, while addig collection and docs', ex);
+  }
+};
+
+export const getCategoriesAndDocuments = async () => {
+  try {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+      const { title, items } = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+
+      return acc;
+    }, {});
+
+    return categoryMap;
+  } catch (ex) {
+    console.error(ex);
+  }
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
