b44a50db0a64fb2c6f8f324ea2a425289b4031d1
Arnaldo Diaz <arnaldo.e.diaz@gmail.com>
2022-08-11 09:21:40 -0400

    fixed bug in setIsCartOpen action creator function

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -130,7 +130,7 @@ export const CartProvider = ({ children }) => {
   };
 
   const setIsCartOpen = (bool) => {
-    dispatch(createAction(CART_ACTION_TYPES.TOGGLE_CART_OPEN, bool));
+    dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool));
   };
 
   const value = {
