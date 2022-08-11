56686e4a9abbe4bd75b2fe4829a70a3d66559720
Arnaldo Diaz <arnaldo.e.diaz@gmail.com>
2022-08-11 09:19:02 -0400

    created cart and user reducers with useReducer hook, moved categories context down the components tree

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
@@ -15,11 +14,9 @@ render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 
+import { CategoriesProvider } from '../../contexts/categories.context';
+
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
-const Shop = () => (
-  <Routes>
-    <Route index element={<CategoriesPreview />} />
-    <Route path=':category' element={<Category />} />
-  </Routes>
-);
+const Shop = () => {
+  return (
+    <CategoriesProvider>
+      <Routes>
+        <Route index element={<CategoriesPreview />} />
+        <Route path=':category' element={<Category />} />
+      </Routes>
+    </CategoriesProvider>
+  );
+};
 
 export default Shop;
